0fbffd065abfdbdd5d4114d3b1a976670264fd30
edvwib <edde275@gmail.com>
2018-03-23 13:00:37 +0100

    include acf plugin in project

--- public/themes/iplay/functions.php
+++ public/themes/iplay/functions.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 // Register plugin helpers.
 require template_path('includes/plugins/plate.php');
 
+// Require Advanced Custom Fields
+require template_path('includes/plugins/acf.php');
+
 add_action('after_setup_theme', function () {
     if (is_admin())
         show_admin_bar(true);
