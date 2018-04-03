a2dbc7dc266edb9f6f783f74424142ec216a8a1a
edvwib <edde275@gmail.com>
2018-04-03 21:45:09 +0200

    setup javascript and scss folder structures

---
+++ resources/assets/styles/components/_footer.scss
@@ -0,0 +1,6 @@
+/*
+ * This file is part of IPLAY.
+ * (c) IPY Holding AB.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
